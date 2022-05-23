7b56474686f74c291a18191ffa9ab8abcefbb326
Nina Louise <ninalouise@reinina.moe>
2022-05-23 16:23:20 +0200

    Change how the script is started Now uses the pythian way of starting scripts using a __name__ condition

--- main.py
+++ main.py
@@ -9,16 +9,12 @@ from src.database import save_battle, has_posted_today  # type: ignore
 
 # TODO: Write the psudeo-code into actual code that functions correctly
 
-def main():
-    # while True:
-    #     if not has_posted_today():
-    #         get_current_battle()
-    #         create_battle_image()
-    #         post()
-    #         save_battle()
+if __name__ == "__main__":
+    while True:
+        # if not has_posted_today():
+        #     get_current_battle()
+        #     create_battle_image()
+        #     post()
+        #     save_battle()
 
-    #     time.sleep(60)
-    return 1
-
-
-main()
+        time.sleep(60)
